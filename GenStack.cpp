125105b09a6898a717e4c2d7e8279b78ef2d4da1
eralbrecht <60952785+eralbrecht@users.noreply.github.com>
2020-04-01 18:30:07 -0700

    Add files via upload

--- GenStack.cpp
+++ GenStack.cpp
@@ -25,6 +25,19 @@ void GenStack::push(char data)
 		//each time we increment and add an element our top will shift over 1
 	}
 }
+char GenStack::peek()
+{
+	if (top < 0)
+	{
+		cout << "the stack is empty -- that doesnt work";
+		//check if empty before popping
+	}
+	else
+	{
+		return myStack[top];
+		//when you evaluate you have to move where the top value is so when we look at the top were ignoring stuff over the top value
+	}
+}
 char GenStack::pop()
 {
 	if (top < 0)
--- main.cpp
+++ main.cpp
@@ -3,12 +3,58 @@ using "GenStack.h"
 int main()
 {
 	countingStack = GenStack::GenStack();
+	int lineCount = 0;
 	for (i = 0; i < analysisString.size(); i++)
 	{
-		currLett = analysisString[i];
-		if (currLett == '[' || currLett == ']' || currLett == '{' || currLett == '}' || currLett == '(' || currLett == ')')
+		char currLett = analysisString[i];
+		if (currLett == '\n')
 		{
-
+			lineCount += 1;
 		}
+		if (currLett == '[' || currLett == '{' || currLett == '(')
+		{
+			countingStack.push(currLett);
+		}
+		if (currLett == ']')
+		{
+			if (countingStack.peek() == '[')
+			{
+				countingStack.pop()
+			}
+			else
+			{
+				//quitfunction
+			}
+		}
+		if (currLett == '}')
+		{
+			if (countingStack.peek() == '{')
+			{
+				countingStack.pop()
+			}
+			else
+			{
+				//quitfunction
+			}
+		}
+		if (currLett == ')')
+		{
+			if (countingStack.peek() == '(')
+			{
+				countingStack.pop()
+			}
+			else
+			{
+				//quitfunction
+			}
+		}
+	}
+	if (countingStack.isEmpty())
+	{
+		//quitfunction
+	}
+	else
+	{
+		//quitfunction with a pop
 	}
 }
